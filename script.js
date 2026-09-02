2b5d87f7243069d81c156014e4c32524382adbc5
dyvcosmetic-ux <dyvcosmetic@gmail.com>
2026-09-02 16:54:10 -0500

    Add new products to script.js

--- script.js
+++ script.js
@@ -194,6 +194,317 @@ const products = [
     badge: "",
     img: "productos/RUBOR MINERALIZADO ANIK.png"
   }
+    // =========================
+  // GLITTER Y ILUMINADORES
+  // =========================
+  {
+    id: 23,
+    name: "Iluminador Planeta",
+    category: "Glitter y iluminadores",
+    price: 15000,
+    badge: "",
+    img: "productos/ILUMINADOR PLANETA.png"
+  },
+  {
+    id: 24,
+    name: "Iluminador Engole",
+    category: "Glitter y iluminadores",
+    price: 16500,
+    badge: "",
+    img: "productos/ILUMINADOR ENGOLE.png"
+  },
+  {
+    id: 25,
+    name: "Iluminador Eclipse Lunar Trendy",
+    category: "Glitter y iluminadores",
+    price: 15000,
+    badge: "",
+    img: "productos/ILUMINADOR ECLIPSE LUNAR TRENDY.png"
+  },
+  {
+    id: 26,
+    name: "Party Glitter Trendy",
+    category: "Glitter y iluminadores",
+    price: 11500,
+    badge: "",
+    img: "productos/PARTY GLITTER TRENDY.png"
+  },
+  {
+    id: 27,
+    name: "Iluminador Dreams Trendy",
+    category: "Glitter y iluminadores",
+    price: 0,
+    badge: "",
+    img: "productos/ILUMINADOR DREAMS TRENDY.png"
+  },
+  {
+    id: 28,
+    name: "Polvo de Hadas Trendy",
+    category: "Glitter y iluminadores",
+    price: 0,
+    badge: "",
+    img: "productos/POLVO DE HADAS TRENDY.png"
+  },
+
+  // =========================
+  // LABIOS
+  // =========================
+  {
+    id: 29,
+    name: "Labial Líquido Hidratante Elaya",
+    category: "Labios",
+    price: 18000,
+    badge: "",
+    img: "productos/LABIAL LÍQUIDO HIDRATANTE ELAYA.png"
+  },
+  {
+    id: 30,
+    name: "Lip Oil Ruby Rose Antigua Presentación",
+    category: "Labios",
+    price: 19000,
+    badge: "",
+    img: "productos/LIP OIL RUBY ROSE ANTIGUA PRESENTACIÓN.png"
+  },
+  {
+    id: 31,
+    name: "Lip Oil Ruby Rose Nueva Presentación",
+    category: "Labios",
+    price: 20000,
+    badge: "",
+    img: "productos/LIP OIL RUBY ROSE NUEVA PRESENTACIÓN.png"
+  },
+  {
+    id: 32,
+    name: "Pureglow - OG",
+    category: "Labios",
+    price: 26000,
+    badge: "",
+    img: "productos/PUREGLOW - OG.png"
+  },
+  {
+    id: 33,
+    name: "Lipstick Engole",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/LIPSTICK ENGOLE.png"
+  },
+  {
+    id: 34,
+    name: "Labial 2 en 1 Elaya",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/LABIAL 2 EN 1 ELAYA.png"
+  },
+  {
+    id: 35,
+    name: "Labial 2 en 1 SLEGLAM",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/LABIAL 2 EN 1 SLEGLAM.png"
+  },
+  {
+    id: 36,
+    name: "Sirenita-Lipgloss",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/SIRENITA-LIPGLOSS.png"
+  },
+  {
+    id: 37,
+    name: "Feeble",
+    category: "Labios",
+    price: 26000,
+    badge: "",
+    img: "productos/FEEBLE.png"
+  },
+  {
+    id: 38,
+    name: "Sirenita Ruby Rose Bálsamo",
+    category: "Labios",
+    price: 16500,
+    badge: "",
+    img: "productos/SIRENITA RUBY ROSE BÁLSAMO.png"
+  },
+  {
+    id: 39,
+    name: "Delineador de Labios Samy",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/DELINEADOR DE LABIOS SAMY.png"
+  },
+  {
+    id: 40,
+    name: "Lip Oil Sandía Trendy",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/LIP OIL SANDIA TRENDY.png"
+  },
+  {
+    id: 41,
+    name: "Just Water Melon Trendy",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/JUST WATER MELON TRENDY.png"
+  },
+  {
+    id: 42,
+    name: "Just Straw Berry Trendy",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/JUST STRAW BERRY TRENDY.png"
+  },
+  {
+    id: 43,
+    name: "Lippie Balm Montoc",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/LIPPIE BALM MONTOC.png"
+  },
+  {
+    id: 44,
+    name: "Lip Balm Bioaqua",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/LIP BALM BIOAQUA.png"
+  },
+  {
+    id: 45,
+    name: "Kit de Brillo + Sombras Barbie Trendy",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/KIT DE BRILLO+SOMBRAS BARBIE TRENDY.png"
+  },
+  {
+    id: 46,
+    name: "Labial Chocolate",
+    category: "Labios",
+    price: 0,
+    badge: "",
+    img: "productos/LABIAL CHOCOLATE.png"
+  },
+
+  // =========================
+  // POLVOS
+  // =========================
+  {
+    id: 47,
+    name: "Polvos Sueltos y Compactos Jack Disney Trendy",
+    category: "Polvos",
+    price: 20000,
+    badge: "",
+    img: "productos/POLVOS SUELTOS Y COMPACTOS JACK DISNEY TRENDY.png"
+  },
+  {
+    id: 48,
+    name: "Polvos Sueltos Banana",
+    category: "Polvos",
+    price: 12000,
+    badge: "",
+    img: "productos/POLVOS SUELTOS BANANA.png"
+  },
+
+  // =========================
+  // VARIEDAD
+  // =========================
+  {
+    id: 49,
+    name: "Mini Blender",
+    category: "Variedad",
+    price: 8500,
+    badge: "",
+    img: "productos/MINI BLENDER.png"
+  },
+  {
+    id: 50,
+    name: "Colageno para Labios",
+    category: "Variedad",
+    price: 2700,
+    badge: "",
+    img: "productos/COLAGENO PARA LABIOS.png"
+  },
+  {
+    id: 51,
+    name: "Colageno para Ojos",
+    category: "Variedad",
+    price: 2700,
+    badge: "",
+    img: "productos/COLAGENO PARA OJOS.png"
+  },
+  {
+    id: 52,
+    name: "Desmaquillante Bi-Fasico",
+    category: "Variedad",
+    price: 25000,
+    badge: "",
+    img: "productos/DESMAQUILLANTE BI-FASICO.png"
+  },
+  {
+    id: 53,
+    name: "Kit de Niacinamida",
+    category: "Variedad",
+    price: 0,
+    badge: "",
+    img: "productos/KIT DE NIACINAMIDA.png"
+  },
+  {
+    id: 54,
+    name: "Kit de Ácido Hialurónico OG",
+    category: "Variedad",
+    price: 0,
+    badge: "",
+    img: "productos/KIT DE ÁCIDO HIALURÓNICO OG.png"
+  },
+  {
+    id: 55,
+    name: "Kit de Mantequilla Corporal + Colonia Arrurru",
+    category: "Variedad",
+    price: 32000,
+    badge: "",
+    img: "productos/KIT DE MANTEQUILLA CORPORAL+COLONIA ARRURRU.png"
+  },
+  {
+    id: 56,
+    name: "Mantequilla Corporal Bubble Gum Sueño Rosa",
+    category: "Variedad",
+    price: 20000,
+    badge: "",
+    img: "productos/MANTEQUILLA CORPORAL BUBBLE GUM SUEÑO ROSA.png"
+  },
+  {
+    id: 57,
+    name: "Cepillo Pulidor",
+    category: "Variedad",
+    price: 7500,
+    badge: "",
+    img: "productos/CEPILLO PULIDOR.png"
+  },
+  {
+    id: 58,
+    name: "Gorro de Satin",
+    category: "Variedad",
+    price: 10200,
+    badge: "",
+    img: "productos/GORRO DE SATIN.png"
+  },
+  {
+    id: 59,
+    name: "Libro de Pestañas Punto a Punto",
+    category: "Variedad",
+    price: 23000,
+    badge: "",
+    img: "productos/LIBRO DE PESTAÑAS PUNTO A PUNTO.png"
+  }
 ];
 
 
